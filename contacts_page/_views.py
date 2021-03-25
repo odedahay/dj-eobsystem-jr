eacf2ec01cefa00dc888974deac5030c53bb40af
Ode Dahay <odedahay@Odes-MacBook-Air.local>
2021-03-25 22:40:57 +0800

    temp removed captcha

--- contacts_page/_views.py
+++ contacts_page/_views.py
@@ -16,9 +16,14 @@ import random
 def contact_us(request):
     products = SystemsProduct.objects.order_by('order_by').filter(is_published=True)
     contact_sidebar = Page.objects.filter(slug__exact='contact-us')
+    num=random.randrange(1121,9899)
     
+    global str_num
+    
+    str_num=str(num)
+
     context = {
-       
+        'img': str_num,
         'products':products,
         'contact_sidebar':contact_sidebar
     }
@@ -35,7 +40,7 @@ def submit(request):
         email = request.POST['email']
         phone = request.POST['phone']
         enquiry = request.POST['enquiry']
-        # cap = request.POST.get("captha")
+        cap = request.POST.get("captha")
         message = request.POST.get('message')
 
         # save to object
@@ -47,34 +52,34 @@ def submit(request):
                 'message' :message,
         } 
 
-        # if str(cap) == str_num:
+        if str(cap) == str_num:
             
-        # Save to DB
-        contact = Contact(name=name, email=email, phone=phone, enquiry=enquiry,  message=message)
-        contact.save()
+            # Save to DB
+            contact = Contact(name=name, email=email, phone=phone, enquiry=enquiry,  message=message)
+            contact.save()
 
-        try:
-            #settings.EMAIL_HOST_USER,
-            mail = EmailMessage(
-                'Email inquiry from ' + name, 
-                # 'Contact Number: ' +  phone + '.'+ '%0A' + message + '\n', 
-                render_to_string('contacts/email.html', contextEmail),
-                email,
-                [settings.EMAIL_HOST_USER], 
-                reply_to=[email]              
-            )
+            try:
+                #settings.EMAIL_HOST_USER,
+                mail = EmailMessage(
+                    'Email inquiry from ' + name, 
+                    # 'Contact Number: ' +  phone + '.'+ '%0A' + message + '\n', 
+                    render_to_string('contacts/email.html', contextEmail),
+                    email,
+                    [settings.EMAIL_HOST_USER], 
+                    reply_to=[email]              
+                )
 
-            mail.content_subtype = 'html'
-            mail.send(fail_silently=False)
-            
-            return redirect('/contact-us/thank-you')
+                mail.content_subtype = 'html'
+                mail.send(fail_silently=False)
+                
+                return redirect('/contact-us/thank-you')
 
-        except BadHeaderError:
-            return HttpResponse('Invalid header found')
-        # else:
+            except BadHeaderError:
+                return HttpResponse('Invalid header found')
+        else:
 
-        #     messages.error(request, 'Captha do not match')
-        #     return redirect('contact-us')
+            messages.error(request, 'Captha do not match')
+            return redirect('contact-us')
     
     else:
 
